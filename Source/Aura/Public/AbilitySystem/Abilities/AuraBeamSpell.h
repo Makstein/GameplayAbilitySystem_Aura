af0497f9d9b70fc5577741fff627b6fae60e3747
Makstein <1105515416@qq.com>
2025-09-04 14:44:26 +0800

    feat: add target tracing to AuraBeamSpell and update related Blueprints
    - Implemented `TraceFirstTarget` method in `AuraBeamSpell` for sphere-trace functionality.
    - Updated Blueprint assets: GA_Electrocute and GC_ShockLoop for compatibility.

--- Source/Aura/Public/AbilitySystem/Abilities/AuraBeamSpell.h
+++ Source/Aura/Public/AbilitySystem/Abilities/AuraBeamSpell.h
@@ -22,6 +22,9 @@ public:
 	UFUNCTION(BlueprintCallable)
 	void StoreVariables();
 
+	UFUNCTION(BlueprintCallable)
+	void TraceFirstTarget(const FVector& BeamTargetLocation);
+
 protected:
 	UPROPERTY(BlueprintReadWrite, Category = "Beam")
 	FVector MouseHitLocation;
